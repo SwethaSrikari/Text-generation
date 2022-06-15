36082282a001dcdeaa0b7aedf7e99a814c6d91a2
Swetha Srikari Maganti <mss@Swethas-MacBook-Air.local>
2022-06-14 20:34:16 -0400

    Changes to init parameters
    Signed-off-by: Swetha Srikari Maganti <mss@Swethas-MacBook-Air.local>

--- models/lstmtrainer.py
+++ models/lstmtrainer.py
@@ -5,18 +5,25 @@ from models.trainer import Trainer
 
 class LSTMTrainer(Trainer):
 	"""
-	Subclass Trainer to implement train_step and valid_step
+	Inherits Trainer and Evaluator class to implement train_step and valid_step
 
-	:param
+	:param model: Model to train
+	:param train_data: Training data
+	:param val_data: Validation data
+	:param optimizer: Optimizer to use when training
+	:param train_loss: Loss function for training
+	:param valid_loss: Loss function for validation
+	:param train_metric: Training metric
+	:param valid_metric: Validation metric
 	"""
 	def __init__(self, model, train_data: tf.data.Dataset, val_data: tf.data.Dataset,
-				 optimizer: tf.keras.optimizers.Optimizer = tf.keras.optimizers.Adam(),
-				 train_loss: tf.keras.losses.Loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-				 valid_loss: tf.keras.losses.Loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-				 train_metric: tf.keras.metrics.Metric = tf.keras.metrics.SparseCategoricalAccuracy(),
-				 valid_metric: tf.keras.metrics.Metric = tf.keras.metrics.SparseCategoricalAccuracy()):
+				 optimizer: tf.keras.optimizers.Optimizer,
+				 train_loss: tf.keras.losses.Loss,
+				 valid_loss: tf.keras.losses.Loss,
+				 train_metric: tf.keras.metrics.Metric,
+				 valid_metric: tf.keras.metrics.Metric):
 
-		super().__init__(model, train_data, optimizer, train_loss, train_metric)
+		super().__init__(model, train_data, val_data, optimizer, train_loss, valid_loss, train_metric, valid_metric)
 
 
 	# @tf.function
@@ -29,13 +36,11 @@ class LSTMTrainer(Trainer):
 		# For each batch, we open a GradientTape() scope
 		with tf.GradientTape() as tape:
 
-			# Compile model
 			# Inside this scope, we call the model (forward pass) and compute the loss
 			logits = self.model.call(train_inputs, training=True)
 
 			loss_value = self.train_loss(train_targets, logits)
 
-		self.optimizer = tf.keras.optimizers.Adam() #### Check why proper arguments are not passed
 		# Outside the scope, we retrieve the gradients of the weights of the model with regard to the loss
 		grads = tape.gradient(loss_value, self.model.trainable_weights)
 
@@ -48,18 +53,18 @@ class LSTMTrainer(Trainer):
 		return loss_value
 
 
-# @tf.function
-def valid_step(self, valid_inputs: tf.Tensor, valid_targets: tf.Tensor):
-	"""
-	Uses trained model to make prediction on validation set
-	:param valid_inputs: validation inputs
-	:param valid_targets: validation labels
-	"""
-	val_logits = self.model(valid_inputs, training=False)
+	# @tf.function
+	def valid_step(self, valid_inputs: tf.Tensor, valid_targets: tf.Tensor):
+		"""
+		Uses trained model to make prediction on validation set
+		:param valid_inputs: validation inputs
+		:param valid_targets: validation labels
+		"""
+		val_logits = self.model(valid_inputs, training=False)
 
-	# Update val metrics
-	self.valid_metric.update_state(valid_targets, val_logits)
-	self.valid_loss(valid_targets, val_logits)
+		# Update val metrics
+		self.valid_metric.update_state(valid_targets, val_logits)
+		self.valid_loss(valid_targets, val_logits)
 
 
 
